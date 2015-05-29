b42b4d38f1f4924e5c857477a5d93032a91c1652
Kyle Rockman <kyle.rockman@mac.com>
2015-05-29 16:26:02 -0500

    flesh out waybill cli
    Signed-off-by: Kyle Rockman <kyle.rockman@mac.com>

--- README.rst
+++ README.rst
@@ -1,4 +1,26 @@
 waybill
 =======
 
-A python commandline tool to run commandline tools that are in docker images
+A python commandline tool to run commandline tools that are housed in docker images to make them like binary installs
+
+Per present day shipping terms:
+A "Waybill" is a non-negotiable document prepared by or on behalf of the carrier at origin. The document shows origin point, destination, route, consignor, consignee, shipment description and amount charged for the transportation service.
+
+Waybill takes this concept and applys it to the docker ecosystem and gives you a commandline interface to create, share and use these shell environment shims to run tools contained in docker images, but automates the docker parts to make it so these docker images act like binary distributions.
+All you do is define the necessary parameters and a shim will be created that you can use in your shell environment
+
+# This is the creation interface for waybills
+waybill create "mycustomtool" "docker.sample.com/mycustomtool:latest"
+
+# This is an alternative command to create that helps you import waybill yaml definitions so you can share them with others easily
+# You could also just share your ~/.waybills directory
+waybill load waybills.yaml
+
+# For more documentation see
+waybill --help
+
+# After the waybills have been defined/setup
+$(waybill shellinit) # this could be run in .bash_rc or .profile or similar
+
+# Now a custom shim should be in place
+mycustomtool --version # will run the shim that runs a docker pull and docker run for the predefined image with the given command name and args
--- waybill/cli.py
+++ waybill/cli.py
@@ -1,6 +1,110 @@
+import os
+import yaml
+from yaml import Loader
+from pyul.coreUtils import DotifyDict
+from battalion.api import *
+
+waybill_template = """
+echo "    - {command}"
+
+function {command}()
+{{
+  docker pull {docker_id}
+  docker run -it --rm -v ~/.{command}:/.{command} "{docker_id}" {command} $@
+}}
+"""
+
+def construct_yaml_map(self, node):
+    # Override the default string handling function 
+    # to always return unicode objects
+    return DotifyDict(self.construct_mapping(node))
+Loader.add_constructor(u'tag:yaml.org,2002:map', construct_yaml_map)
+
+def which(program):
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and (os.access(fpath, os.X_OK) or os.access(fpath, os.F_OK))
+
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            path = path.strip('"')
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+
+    return None
+
+
+class waybill(CLI):
+    
+    class State:
+        version = "0.0.1"
+
+    def get_waybill_dir(self):
+        return os.path.dirname(self.state.config_file)
+
+    def get_waybills(self):
+        waybill_dir = self.get_waybill_dir()
+        if os.path.exists(waybill_dir):
+            files = [os.path.join(waybill_dir, f) for f in os.listdir(waybill_dir)]
+            for f in [f for f in files if os.path.isfile(f) and os.path.splitext(f)[-1] == '.waybill']:
+                yield f
+
+    @command
+    def create(cli, command, docker_id):
+        """Creates waybill shims from a given command name and docker image"""
+        content = waybill_template.format(command=command,
+                                          docker_id=docker_id)
+        waybill_dir = cli.get_waybill_dir()
+        waybill_filename = os.path.join(waybill_dir, command + '.waybill')
+        with open(waybill_filename, 'wb') as filehandle:
+            filehandle.write(content)
+        cli.log.info('Created waybill {0}'.format(waybill_filename))
+
+    @command
+    def load(cli, yaml_filename):
+        """Creates waybill shims from a given yaml file definiations"""
+        """Expected Definition:
+        - name: NAME
+          docker_id: IMAGE
+        - name: NAME
+          docker_id: IMAGE
+        """
+        with open(yaml_filename, 'rb') as filehandle:
+            for waybill in yaml.load(filehandle.read()):
+                cli.create(waybill.name,
+                           waybill.docker_id)
+
+    @command
+    def list(cli):
+        """Prints out the list of known waybills"""
+        for waybill in cli.get_waybills():
+            cli.log.info(waybill)
+
+    @command
+    def clear(cli):
+        for waybill in cli.get_waybills():
+            cli.log.info('Removing waybill {0}'.format(waybill))
+            os.remove(waybill)
+
+    @command
+    def shellinit(cli):
+        """Implements the waybill shims in the active shell"""
+        output = 'eval echo "Initializing Waybills"'
+        if which('docker') is None:
+            raise ValueError("Unable to find program 'docker'.  Please make sure it is installed and setup properly")
+        for waybill in cli.get_waybills():
+            output += ' && source {0}'.format(waybill)
+        return output
+            
+
 
 def main():
-    pass
+    waybill.main()
+
 
 if __name__ == "__main__":
     main()
